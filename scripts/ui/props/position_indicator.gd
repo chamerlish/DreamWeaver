b01fd07e2676018e3a340a2d09e82927bd6da42b
chamerli <fares.setbel@proton.me>
2026-04-22 10:42:10 +0100

    little small animation

--- scripts/ui/props/position_indicator.gd
+++ scripts/ui/props/position_indicator.gd
@@ -7,3 +7,14 @@ class_name PositionIndicator
 	set(value):
 		color = value
 		modulate = value
+
+var time := 0.0
+var speed := randf_range(1.0, 3.0)
+var amplitude := 0.1
+var base_scale := Vector3.ONE
+
+func _process(delta):
+	time += delta * speed
+	
+	var s = 1.0 + sin(time) * amplitude
+	scale = base_scale * s
